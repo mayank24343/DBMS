d876871c030e8f5df742199625b24f5b5384a53d
mayank24343 <mayank24343@iiitd.ac.in>
2026-02-19 13:20:01 +0530

    Change citizen and contact tables to use BIGINT UNSIGNED
    Updated data types for several columns to use BIGINT UNSIGNED for improved range and integrity. Adjusted foreign key references accordingly.

--- group29schema.sql
+++ group29schema.sql
@@ -16,7 +16,7 @@ CREATE TABLE citizen (
 
 CREATE TABLE citizen_contact (
     id SERIAL PRIMARY KEY,
-   	citizen_id INT,
+   	citizen_id BIGINT UNSIGNED,
     email VARCHAR(100),
     phone CHAR(10),
 	CHECK ((phone IS NOT NULL AND email IS NULL) OR (phone IS NULL AND email IS NOT NULL)),
@@ -40,7 +40,7 @@ CREATE TABLE health_facility (
 
 CREATE TABLE healthfac_contact (
     id SERIAL PRIMARY KEY,
-   	healthfac_id INT,
+   	healthfac_id BIGINT UNSIGNED,
     email VARCHAR(100),
     phone CHAR(10),
 	CHECK ((phone IS NOT NULL AND email IS NULL) OR (phone IS NULL AND email IS NOT NULL)),
@@ -63,7 +63,7 @@ CREATE TABLE supplier (
 
 CREATE TABLE supplier_contact (
     id SERIAL PRIMARY KEY,
-   	supplier_id INT,
+   	supplier_id BIGINT UNSIGNED,
     email VARCHAR(100),
     phone CHAR(10),
 	CHECK ((phone IS NOT NULL AND email IS NULL) OR (phone IS NULL AND email IS NOT NULL)),
@@ -81,9 +81,9 @@ CREATE TABLE item (
 
 CREATE TABLE listing (
     id SERIAL PRIMARY KEY,
-    supplier_id INT NOT NULL,
-    item_id INT NOT NULL,
-    quantity INT NOT NULL,
+    supplier_id BIGINT UNSIGNED NOT NULL,
+    item_id BIGINT UNSIGNED NOT NULL,
+    quantity BIGINT UNSIGNED NOT NULL,
     price_per_item DECIMAL(10,2) NOT NULL,
 	FOREIGN KEY (supplier_id) REFERENCES supplier(id) ON DELETE CASCADE,
 	FOREIGN KEY (item_id) REFERENCES item(id) ON DELETE CASCADE
@@ -98,14 +98,14 @@ CREATE TABLE healthcareworker (
 
 CREATE TABLE skills (
     id SERIAL PRIMARY KEY,
-    worker_id INT,
+    worker_id BIGINT UNSIGNED,
     name VARCHAR(100) NOT NULL,
     type VARCHAR(30) NOT NULL CHECK (type IN ('qualification','specialization')),
 	FOREIGN KEY (worker_id) REFERENCES healthcareworker(id) ON DELETE CASCADE
 );
 
 CREATE TABLE attendance (
-    worker_id INT,
+    worker_id BIGINT UNSIGNED,
     att_date DATE NOT NULL,
     PRIMARY KEY (worker_id, att_date),
 	FOREIGN KEY (worker_id) REFERENCES healthcareworker(id) ON DELETE CASCADE
@@ -113,8 +113,8 @@ CREATE TABLE attendance (
 
 CREATE TABLE visit (
     id SERIAL PRIMARY KEY,
-    citizen_id INT,
-    centre_id INT,
+    citizen_id BIGINT UNSIGNED,
+    centre_id BIGINT UNSIGNED,
     visit_date DATE NOT NULL,
     reason TEXT,
 	FOREIGN KEY (citizen_id) REFERENCES citizen(citizen_id),
@@ -122,11 +122,12 @@ CREATE TABLE visit (
 );
 
 CREATE TABLE doctor_visit (
-    visit_id INT,
-    doctor_id INT REFERENCES healthcareworker(id),
+    visit_id BIGINT UNSIGNED,
+    doctor_id BIGINT UNSIGNED,
     role VARCHAR(50) NOT NULL,
     PRIMARY KEY (visit_id, doctor_id),
-	FOREIGN KEY (visit_id) REFERENCES visit(id) ON DELETE CASCADE
+	FOREIGN KEY (visit_id) REFERENCES visit(id) ON DELETE CASCADE,
+	FOREIGN KEY (doctor_id) REFERENCES healthcareworker(id)
 );
 
 CREATE TABLE diagnosis (
@@ -138,8 +139,8 @@ CREATE TABLE diagnosis (
 
 CREATE TABLE prescription (
     id SERIAL PRIMARY KEY,
-    visit_id INT,
-    item_id INT,
+    visit_id BIGINT UNSIGNED,
+    item_id BIGINT UNSIGNED,
     item_type VARCHAR(10) CHECK (item_type IN ('medicine','vaccine')),
     dosage VARCHAR(50),
     frequency VARCHAR(50),
@@ -166,9 +167,9 @@ CREATE TABLE lab_test (
 
 CREATE TABLE lab_order (
     id SERIAL PRIMARY KEY,
-    visit_id INT,
-    test_id INT,
-    lab_id INT,
+    visit_id BIGINT UNSIGNED,
+    test_id BIGINT UNSIGNED,
+    lab_id BIGINT UNSIGNED,
     order_date DATE NOT NULL,
 	FOREIGN KEY (visit_id) REFERENCES visit(id),
 	FOREIGN KEY (test_id) REFERENCES lab_test(id),
@@ -177,23 +178,23 @@ CREATE TABLE lab_order (
 
 CREATE TABLE lab_result (
     id SERIAL PRIMARY KEY,
-    order_id INT,
+    order_id BIGINT UNSIGNED,
     result_date DATE,
     result VARCHAR(50),
 	FOREIGN KEY (order_id) REFERENCES lab_order(id)
 );
 
 CREATE TABLE lab_test_provided (
-	test_id INT,
-	fac_id INT,
+	test_id BIGINT UNSIGNED,
+	fac_id BIGINT UNSIGNED,
 	PRIMARY KEY(test_id,fac_id),
 	FOREIGN KEY (test_id) REFERENCES lab_test(id),
 	FOREIGN KEY (fac_id) REFERENCES health_facility(id)
 );
 
 CREATE TABLE procedure_provided (
-	procedure_id INT,
-	fac_id INT REFERENCES health_facility(id),
+	procedure_id BIGINT UNSIGNED,
+	fac_id BIGINT UNSIGNED,
 	PRIMARY KEY(procedure_id,fac_id),
 	FOREIGN KEY (fac_id) REFERENCES health_facility(id),
 	FOREIGN KEY (procedure_id) REFERENCES medical_procedure(procedure_id)
@@ -201,11 +202,11 @@ CREATE TABLE procedure_provided (
 
 CREATE TABLE vaccination (
     id SERIAL PRIMARY KEY,
-    citizen_id INT,
-    vaccine_id INT,
+    citizen_id BIGINT UNSIGNED,
+    vaccine_id BIGINT UNSIGNED,
     vaccination_date DATE NOT NULL,
     dose_no INT NOT NULL,
-    centre_id INT,
+    centre_id BIGINT UNSIGNED,
 	FOREIGN KEY (citizen_id) REFERENCES citizen(citizen_id),
 	FOREIGN KEY (vaccine_id) REFERENCES item(id),
 	FOREIGN KEY (centre_id) REFERENCES health_facility(id)
@@ -213,7 +214,7 @@ CREATE TABLE vaccination (
 
 CREATE TABLE wards (
     id SERIAL PRIMARY KEY,
-    facility_id INT,
+    facility_id BIGINT UNSIGNED,
     type VARCHAR(30) NOT NULL DEFAULT 'Normal',
 	FOREIGN KEY (facility_id) REFERENCES health_facility(id)
 );
@@ -227,8 +228,8 @@ CREATE TABLE bed (
 
 CREATE TABLE admission (
     id SERIAL PRIMARY KEY,
-    citizen_id INT,
-    visit_id INT,
+    citizen_id BIGINT UNSIGNED,
+    visit_id BIGINT UNSIGNED,
     admission_date DATE NOT NULL,
     discharge_date DATE,
 	FOREIGN KEY (citizen_id) REFERENCES citizen(citizen_id),
@@ -237,8 +238,8 @@ CREATE TABLE admission (
 
 CREATE TABLE bed_allocs (
     id SERIAL PRIMARY KEY,
-    bed_id INT,
-    adm_id INT,
+    bed_id BIGINT UNSIGNED,
+    adm_id BIGINT UNSIGNED,
     start_time TIMESTAMP NOT NULL,
     end_time TIMESTAMP,
 	FOREIGN KEY (bed_id) REFERENCES bed(id),
@@ -258,7 +259,7 @@ CREATE TABLE warehouse (
 
 CREATE TABLE warehouse_inventory (
     id SERIAL PRIMARY KEY,
-    warehouse_id INT,
+    warehouse_id BIGINT UNSIGNED,
     item_id INT NOT NULL,
     quantity INT NOT NULL,
     expiry DATE NOT NULL,
@@ -268,7 +269,7 @@ CREATE TABLE warehouse_inventory (
 
 CREATE TABLE facility_inventory (
     id SERIAL PRIMARY KEY,
-    facility_id INT,
+    facility_id BIGINT UNSIGNED,
     item_id INT NOT NULL,
     quantity INT NOT NULL,
     expiry DATE NOT NULL,
@@ -278,11 +279,11 @@ CREATE TABLE facility_inventory (
 
 CREATE TABLE supply_order (
     id SERIAL PRIMARY KEY,
-    supplier_id INT,
-    destination_id INT NOT NULL,
+    supplier_id BIGINT UNSIGNED,
+    destination_id BIGINT UNSIGNED NOT NULL,
     destination_type VARCHAR(10) CHECK (destination_type IN ('warehouse','facility')),
-    item_id INT NOT NULL,
-    quantity INT NOT NULL,
+    item_id BIGINT UNSIGNED NOT NULL,
+    quantity BIGINT UNSIGNED NOT NULL,
     order_date DATE NOT NULL,
     status VARCHAR(20) NOT NULL DEFAULT 'Order Placed',
 	CHECK (status IN ('Order Placed','Received','Cancelled')),
@@ -295,11 +296,12 @@ CREATE TABLE disease_case (
     disease VARCHAR(100) NOT NULL,
     region VARCHAR(100) NOT NULL,
     report_date DATE NOT NULL,
-    worker_id INT,
-    patient_id INT,
+    worker_id BIGINT UNSIGNED,
+    patient_id BIGINT UNSIGNED,
 	FOREIGN KEY (worker_id) REFERENCES healthcareworker(id),
 	FOREIGN KEY (patient_id) REFERENCES citizen(citizen_id)
 );
+
 
 
 
