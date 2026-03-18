0797f9f5b0b2b5c485f4fafcdfcbbbf1bbede09e
Anup Dev Pattnaik <anup24091@iiitd.ac.in>
2026-03-19 00:17:49 +0530

    Add API view for ward availability

--- backend/facilities/views.py
+++ backend/facilities/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Wards
 
-# Create your views here.
+@api_view(['GET'])
+def get_ward_availability(request, fac_id):
+    wards = Wards.objects.filter(facility_id=fac_id)
+    data = []
+    for ward in wards:
+        data.append({
+            "id": ward.id,
+            "type": ward.type,
+            "total": ward.total,
+            "occupied": ward.occupied,
+            # Calculate available beds right here on the backend
+            "available": ward.total - ward.occupied 
+        })
+    return Response(data)
